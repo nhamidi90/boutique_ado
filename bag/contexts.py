8f1e35f232b61fa92c73966f0dbcd337115b24bf
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-06-04 12:23:39 +0000

    Updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,29 @@
 from decimal import Decimal
 from django.conf import settings 
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    # iterate through all items in shopping bag 
+    for item_id, quantity in bag.items():
+        # get the product
+        product = get_object_or_404(Product, pk=item_id)
+        # add to the total cost
+        total += quantity * product.price
+        # add to the product count
+        product_count += quantity
+        #append items to bag
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     # calculate shipping costs
     if total < settings.FREE_DELIVERY_THRESHOLD:
