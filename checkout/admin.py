0a83f8576cf53e75dd1c70f23b63a3a2aed1f83d
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-06-06 10:53:26 +0000

    Add forms, models and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = ('order_number', 'date', 
+                       'delivery_cost', 'order_total', 
+                       'grand_total')
+
+    # not necessary but allows us to specify order of fields in admin
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total')
+
+    # restrict the columns that show up in the order list
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost', 'grand_total')
+    
+    # order by date in reverse chronological order
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/signals.py
@@ -0,0 +1,22 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+# to execute the function any time the post_save signal is sent, use receiver
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    #instance.order refers to the order this specific line item is related to
+    instance.order.update_total()
+
+# update totals when item is deleted
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    #instance.order refers to the order this specific line item is related to
+    instance.order.update_total()
