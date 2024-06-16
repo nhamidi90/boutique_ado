a3fd02f6b9f29ddc4b4eb3beccf378f1ec31ac1c
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-06-16 10:20:02 +0000

    Deploy app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 """
 Django settings for boutique_ado project.
@@ -30,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '8000-nhamidi90-boutiqueado-sfzd0c4csq1.ws.codeinstitute-ide.net',
     'localhost'
+    'https://boutique-ado-nhamidi-3e4fde1aef0b.herokuapp.com/'
 ]
 
 
@@ -117,6 +119,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
@@ -124,13 +127,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
